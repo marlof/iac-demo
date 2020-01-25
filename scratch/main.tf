c796d3a324279bb4de30d6a0943b1c0ea494f3ed
lee <lee.a.howard@hotmail.co.uk>
2020-01-25 23:33:59 +0000

    Make vars more useful, call app module

--- scratch/main.tf
+++ scratch/main.tf
@@ -1,23 +1,36 @@
+# Variables for user/pass for k8s cluster and path to GCP creds file
 variable "un" {
-  default = "admin-user"
-}
-variable "pw" {
-  default = "averycomplicatedpassword"
+  type = string
+  description = "User name to set for k8s cluster"
 }
 
+variable "pw" {
+    type = string
+    description = "Password to set for k8s cluster"
+}
+
+variable "creds" {
+    type = string
+    description = "Path to GCP credentials JSON file"
+}
+
+
+# Define k8s cluster
 module "cluster" {
   source   = "./cluster"
   un = var.un
   pw = var.pw
+  creds = var.creds
 }
 
-#module "app" {
-#  source   = "./app"
-#  host     = module.cluster.endpoint
-#  un = var.un
-#  pw = var.pw
-#
-#  client_cert     = module.cluster.client_cert
-#  client_key      = module.cluster.client_key
-#  cluster_ca_cert = module.cluster.cluster_ca_cert
-#}
+# Define db, api and front end components
+module "app" {
+  source   = "./app"
+  un = var.un
+  pw = var.pw
+
+  endpoint        = module.cluster.endpoint
+  client_cert     = module.cluster.client_cert
+  client_key      = module.cluster.client_key
+  cluster_ca_cert = module.cluster.cluster_ca_cert
+}
