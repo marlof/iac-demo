8c2a2f3593bdf1d137ac3c82498cfc4d87996a63
lee <lee.a.howard@hotmail.co.uk>
2020-01-25 11:34:11 +0000

    Switch to a GKE cluster

--- scratch/main.tf
+++ scratch/main.tf
@@ -6,30 +6,9 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-      nat_ip = google_compute_address.vm_static_ip.address
-    }
-  }
-
-}
-
-resource "google_compute_address" "vm_static_ip" {
-  name = "terraform-static-ip"
+resource "google_container_cluster" "gke-cluster" {
+  name               = "spoved-iac-demo-cluster"
+  network            = "default"
+  location           = "europe-west1-b"
+  initial_node_count = 3
 }
