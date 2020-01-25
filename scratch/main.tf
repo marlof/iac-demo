1fb8d8201e9f31884a0e65a6a4a7ef4ecf873249
lee <lee.a.howard@hotmail.co.uk>
2020-01-25 10:29:17 +0000

    Test Terraform config

---
+++ scratch/main.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  credentials = file("../../terraform-test.json")
+
+  project = "spoved-iac-demo"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
