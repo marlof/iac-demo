bb261c288b61cc2601d6a60513a71267b8615420
lee <lee.a.howard@hotmail.co.uk>
2020-01-25 11:11:15 +0000

    Add static IP

--- scratch/main.tf
+++ scratch/main.tf
@@ -13,16 +13,23 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
     }
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = google_compute_network.vpc_network.self_link
     access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
+
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
 }
